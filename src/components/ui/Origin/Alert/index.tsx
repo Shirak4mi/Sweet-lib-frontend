deb16838534b3a7d6af2c9af183e5914457378b6
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-17 12:50:56 -0400

    +adds

--- src/components/ui/Origin/Alert/index.tsx
+++ src/components/ui/Origin/Alert/index.tsx
@@ -24,11 +24,6 @@ export default function BaseOriginAlert({ show, neon, details, type, title, acti
           <AlertIcon Type={type} />
           <div className={details ? "grow space-y-1" : "flex grow justify-between gap-3"}>
             <p className={details ? "text-sm font-medium" : "text-sm"}>{title}</p>
-            {action && (
-              <a href="#" className="group whitespace-nowrap text-sm font-medium" onClick={func}>
-                {label}
-              </a>
-            )}
             {details && (
               <ul className="list-inside list-disc text-sm text-muted-foreground">
                 {details.map((item, key) => (
@@ -37,6 +32,11 @@ export default function BaseOriginAlert({ show, neon, details, type, title, acti
               </ul>
             )}
           </div>
+          {action && (
+            <a href="#" className="group whitespace-nowrap text-sm font-medium" onClick={func}>
+              {label}
+            </a>
+          )}
         </div>
       </div>
     )
