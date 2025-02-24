efff53a18311ff24999740c9e65072111340014d
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 03:39:42 -0400

    +adds

---
+++ src/components/ui/forms/books/New.tsx
@@ -0,0 +1,5 @@
+import { type ReactNode } from "react";
+
+export default function NewBook(): ReactNode {
+  return <div>CreateNewBook</div>;
+}
---
+++ src/components/ui/forms/users/Register.tsx
@@ -0,0 +1,5 @@
+import { type ReactNode } from "react";
+
+export default function RegisterUser(): ReactNode {
+  return <div>RegisterUser</div>;
+}
