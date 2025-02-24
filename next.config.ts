7429d8d54c16c25a731611a53e0edd8d6ff12fda
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 14:44:31 -0400

    +adds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextIntl = createNextIntlPlugin();
 
-export default nextConfig;
+export default withNextIntl({
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+      },
+    ],
+  },
+  redirects: async () => [
+    { source: "/", destination: "/en/Home", permanent: true },
+    { source: "/en", destination: "/en/Home", permanent: true },
+    { source: "/es", destination: "/es/Home", permanent: true },
+  ],
+}) as NextConfig;
