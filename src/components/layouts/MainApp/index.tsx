b6f640d321031c4d6dd4a1b501cfe1fdd6d6433a
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 18:15:11 -0400

    +adds

--- src/components/layouts/MainApp/index.tsx
+++ src/components/layouts/MainApp/index.tsx
@@ -1,13 +1,19 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import { type ReactNode } from "react";
 import Navbar from "./Navbar.tsx";
 
 import type { IRSC } from "@/types/components";
 
-export default function MainAppWrapper({ children }: IRSC): ReactNode {
+export default async function MainAppWrapper({ children }: IRSC): Promise<ReactNode> {
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
   return (
-    <>
+    <NextIntlClientProvider messages={messages}>
       <Navbar />
       <main>{children}</main>
-    </>
+    </NextIntlClientProvider>
   );
 }
