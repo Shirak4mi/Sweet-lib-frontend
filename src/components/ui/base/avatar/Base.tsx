69f070ed5d9f5fcf94ef12068ced5ed9f1f0d9d6
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 12:03:14 -0400

    +adds

---
+++ src/components/ui/base/avatar/Base.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Root, Image, Fallback } from "@radix-ui/react-avatar";
+import { cn } from "@/utils/functions";
+
+import type { ComponentProps, ReactNode } from "react";
+
+export function Avatar({ className, ...props }: ComponentProps<typeof Root>): ReactNode {
+  return (
+    <Root
+      className={cn("relative flex size-8 shrink-0 overflow-hidden rounded-full", className)}
+      data-slot="avatar"
+      {...props}
+    />
+  );
+}
+
+export function AvatarImage({ className, ...props }: ComponentProps<typeof Image>): ReactNode {
+  return <Image data-slot="avatar-image" className={cn("aspect-square size-full", className)} {...props} />;
+}
+
+export function AvatarFallback({ className, ...props }: ComponentProps<typeof Fallback>): ReactNode {
+  return (
+    <Fallback
+      className={cn("bg-muted flex size-full items-center justify-center rounded-full", className)}
+      data-slot="avatar-fallback"
+      {...props}
+    />
+  );
+}
