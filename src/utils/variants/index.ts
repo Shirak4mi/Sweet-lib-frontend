c452c5af97c06716fbe507c5d462b1b0ac8be08c
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 12:03:25 -0400

    +adsd

--- src/utils/variants/index.ts
+++ src/utils/variants/index.ts
@@ -1,7 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-/* Base variants & types */
-
 export const alertVariants = cva(
   "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
   {
@@ -66,6 +64,25 @@ export const toggleVariants = cva(
 );
 
 export type TToggleVariants = VariantProps<typeof toggleVariants>;
-/* Base variants & types */
+
+export const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-full border px-2.5 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 gap-1 [&>svg]:pointer-events-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 [&>svg]:shrink-0 leading-normal select-none backdrop-blur-[2px]",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent bg-primary/95 text-primary-foreground hover:bg-primary/90 transition-colors",
+        secondary: "border-transparent bg-secondary/95 text-secondary-foreground hover:bg-secondary/90 transition-colors",
+        destructive:
+          "border-transparent bg-destructive/95 text-destructive-foreground hover:bg-destructive/90 transition-colors",
+        outline: "text-foreground/95 hover:bg-accent hover:text-accent-foreground transition-colors",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export type TBadgeVariants = VariantProps<typeof badgeVariants>;
 
 export * from "./animated.ts";
