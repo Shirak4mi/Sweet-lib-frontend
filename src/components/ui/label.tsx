af2d6049ef5fcc392b5b849afc25a9a9b48a30aa
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-24 12:03:19 -0400

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,13 +1,12 @@
 "use client";
+import { Root } from "@radix-ui/react-label";
+import { cn } from "@/utils/functions";
 
-import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
+import type { ComponentProps, ReactNode } from "react";
 
-import { cn } from "@/utils";
-
-function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+export function Label({ className, ...props }: ComponentProps<typeof Root>): ReactNode {
   return (
-    <LabelPrimitive.Root
+    <Root
       data-slot="label"
       className={cn(
         "text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
@@ -17,5 +16,3 @@ function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimiti
     />
   );
 }
-
-export { Label };
